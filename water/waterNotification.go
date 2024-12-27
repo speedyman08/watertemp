f6696e05277e37f9c8faac8260bf68f1d035401b
stephen <stephen.jr@selyuk.com>
2024-12-27 23:43:32 +0200

    Improve notification

--- water/waterNotification.go
+++ water/waterNotification.go
@@ -7,7 +7,7 @@ import (
 
 func Notify(temp float64, app fyne.App) {
 	if math.Round(temp) > 45 {
-		var notif = fyne.NewNotification("Water tank", "Water tank temperature has exceeded 45 degrees. Maybe turn the heater off?")
+		var notif = fyne.NewNotification("Water Temperature Warning", "Water tank temperature has exceeded 45 degrees. Maybe turn the heater off?")
 		app.SendNotification(notif)
 	}
 }
