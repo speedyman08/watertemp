91d424e68e2ef2629dfd3ca16d442c7a99656829
speedyman77 <stephen.jr@selyuk.com>
2025-01-12 17:22:19 +0200

    adjust server death to 480

--- water/status.go
+++ water/status.go
@@ -2,6 +2,7 @@ package water
 
 import "fmt"
 
+const deathTime = 480 // 8 minutes
 type Status struct {
 	Temperature               float64
 	LastSuccessfulPollSeconds int64 // in seconds
@@ -9,7 +10,7 @@ type Status struct {
 
 func (s Status) String() string {
 	var statusStr = ""
-	if s.LastSuccessfulPollSeconds > 120 || s.Temperature == 0 {
+	if s.LastSuccessfulPollSeconds > deathTime || s.Temperature == 0 {
 		statusStr = "Server is 100% dead"
 	} else {
 		statusStr = fmt.Sprintf("%d seconds ago", s.LastSuccessfulPollSeconds)
